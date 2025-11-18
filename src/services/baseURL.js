da56d827776e049a65550c71cf353734c5c343fa
shahansv <shahanvsaleem@gmail.com>
2025-11-18 15:11:55 +0530

    live dummy server added

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,3 +1,3 @@
-export const collegeURL = "http://universities.hipolabs.com/search?country=";
+export const collegeURL = "https://universities.hipolabs.com/search?country=";
 // export const baseURL = "http://localhost:3000";
 export const baseURL = "https://unitrack-dummy-server.onrender.com";
